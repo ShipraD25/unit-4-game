82048e1b1dfba8a3d1d773a9f58daf82ccca61e8
Shipra <drshipradyundi@gmail.com>
2019-10-16 13:38:15 -0700

    adding html and images in the assets folder

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// global variables
+var wins = 0;
+var losses = 0;
+userScore = 0;
+var num1 = 0;
+var num2 = 0;
+var num3 = 0;
+var num4 = 0;
+
+
+var targetScore = Math.floor(Math.random() * 120) + 19;
+console.log(random);
+
+var crystalValue = math.floor(Math.random() * 12) + 1;
+
+// $("#crystal-button").on("click", function() {
+
+$(document).ready(function() {})
+    //display random number on screen 
+    // on the right wins =0 losses = 0
+    //four crystsl image button below the number
+    //as the user presses any crystal button generate a random number and add it to the players score
+    //as the user presses next crystal another no. is generated and added to the players score
+    // if players score is === to the random no. user wins and another random no. is generated and displayed on the screen
+    //wins = 1
+    // if the players score is greater than random no. user losses and losses go up and new no. is generated and displayed.
