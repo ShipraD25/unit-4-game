a3a6ad9e6ae9d49cf7c49c573b11ee46cbd0f21b
Shipra <drshipradyundi@gmail.com>
2019-10-19 00:52:36 -0700

    adding style.css

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # unit-4-game
+
+## Summary
+This is a simple game in which the user has to match his score with the random number presented on the screen.
+User will be able to achieve this by clicking the available four crystals. These crystals have values that are hidden from user until he/she clicks it. 
+For each round the user will receive a new random target score as well as crystal will be assigned a new value.
+The user will win if his total score matches with the random target score or the user can loose his total score is greater than the target score.
+Wins and losses will go up accordingly.
+
+Pseudocode has been put to understand the functionality of code.
+
+## Tech Used
+* Html
+* css
+* javascript
+* jquery
+
+## javascript code snippet
+```javascript
+ $(".crystal-button").on("click", function() {
+
+        var index = $(this).attr("crystalnumber")
+
+        userScore = userScore + crystals[index]
+
+        $("#user-score").text(userScore);
+
+        // verify
+        if (userScore === targetScore) {
+            wins++;
+            $("#wintext").text(wins);
+            play()
+        } else if (userScore > targetScore) {
+            losses++;
+            $("#lossestext").text(losses);
+            play()
+        }
+        ```
+
+        ## Game Preview
+
+        
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,57 @@
 $(document).ready(function() {
 
-        // global variables
-        var wins = 0;
-        var losses = 0;
-        var userScore = 0;
-        var targetScore = 0
-        var crystals = []
+    // global variables.
+    var wins = 0;
+    var losses = 0;
+    var userScore = 0;
+    var targetScore = 0
+    var crystals = []
 
-        // var num1 = 0;
-        // var num2 = 0;
-        // var num3 = 0;
-        //var num4 = 0;
 
-        function play() {
-            userScore = 0
-            $("#user-score").text(userScore);
-            targetScore = Math.floor(Math.random() * 120) + 19;
-            console.log(targetScore);
-            $("#target-num").text(targetScore);
-            //show in the browser the number
-            // calculate 4 randon for the crystals
-            crystals = []
+    // declaring a function that will restart the game everytime it's called.
+    function play() {
+        userScore = 0
+            //creating random target number.
+        $("#user-score").text(userScore);
+        targetScore = Math.floor(Math.random() * 120) + 19;
+        $("#target-num").text(targetScore);
 
-            // while  ///  until the lenght is = 4
-            // indexof  using to look for a value inside of an array if value is inside give you the postion if not return -1
+        // calculate 4 randon values for the crystals.
+        crystals = []
 
-            for (var i = 0; i < 4; i++) {
-                crystals[i] = Math.floor(Math.random() * 12) + 1;
-            }
-            console.log(crystals);
-
-            //while (crystals.length < 4){
-            //   var random = 
-            // verify with indexof if the value is existing 
-            // if not push the value to the array
-            // if yes keep going 
+        for (var i = 0; i < 4; i++) {
+            crystals[i] = Math.floor(Math.random() * 12) + 1;
         }
 
 
-
-        $(".crystal-button").on("click", function() {
-            console.log(this)
-            var index = $(this).attr("crystalnumber")
-            console.log(index, crystals[index])
-            userScore = userScore + crystals[index]
-            $("#user-score").text(userScore);
-            console.log("userscore: ", userScore)
-                // verify
-            if (userScore === targetScore) {
-                wins++;
-                $("#wintext").text(wins);
-                play()
-            } else if (userScore > targetScore) {
-                losses++;
-                $("#lossestext").text(losses);
-                play()
-            }
-
-        })
-
-        play()
+    }
 
 
+    // adding event listenser and also the steps to take after the click event.
+    $(".crystal-button").on("click", function() {
 
-        //display random number on screen 
-        // on the right wins =0 losses = 0
-        //four crystsl image button below the number
-        //as the user presses any crystal button generate a random number and add it to the players score
-        //as the user presses next crystal another no. is generated and added to the players score
-        // if players score is === to the random no. user wins and another random no. is generated and displayed on the screen
-        //wins = 1
-    }) // if the players score is greater than random no. user losses and losses go up and new no. is generated and displayed.
+        var index = $(this).attr("crystalnumber")
+
+        userScore = userScore + crystals[index]
+
+        $("#user-score").text(userScore);
+
+        // verify
+        if (userScore === targetScore) {
+            wins++;
+            $("#wintext").text(wins);
+            play()
+        } else if (userScore > targetScore) {
+            losses++;
+            $("#lossestext").text(losses);
+            play()
+        }
+
+    })
+
+    play()
+
+
+
+
+})
